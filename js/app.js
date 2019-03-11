2b5be5661eb197390ffb12d6cb16c27a2cd6cda0
chiamakaobitube <obitubechiamaka@gmail.com>
2019-03-11 14:57:45 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", () => {
+    let long;
+    let lat;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const api = 'https://api.darksky.net/forecast/6ac6a1c43cbc37bd8ee528388f3882c7/$(lat), $(long)';
+        });
+    } else {
+        h2.textContent = "Enable location";
+    }
+});
